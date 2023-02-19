e076feb8879a2aeddf8365ed532a532c2dcbd0b4
mtdem <mtdemirak@gmail.com>
2023-02-19 13:28:15 -0500

    restructure

---
+++ bootstrap-project-functions.js
@@ -0,0 +1,73 @@
+// fields to validate/reset
+// username (string): usernameInput
+// first name (string): fistNameInput
+// last name (string): lastNameInput
+// phone number (string): phoneNumberInput
+// fax (string): faxInput
+// email (string): emailInput
+// adults (number): adults-dropdown
+// checkin (date): datepicker-checkin
+// checkout (date): datepicker-checkout
+// days (number): daysInput
+// cost (number): costInput
+// message (string): msg
+// range (number): rangeInput
+// priority (string):
+//    low:    lowprty
+//    medium: medprty
+//    high:   highprty
+// reset button: reset-inputs
+// submit button: submit-inputs
+
+var inputs = {}
+inputs.username = $('#usernameInput').val()
+inputs.firstname = $('#firstNameInput').val()
+inputs.lastName = $('#lastNameInput').val()
+inputs.phoneNumber = $('#phoneNumberInput').val()
+inputs.fax = $('#faxInput').val()
+inputs.email = $('#emailInput').val()
+inputs.adults = $('#adults-dropdown').val()
+inputs.checkin = $('#datepicker-checkin').val()
+inputs.checkout = $('#datepicker-checkout').val()
+inputs.days = $('#daysInput').val()
+inputs.cost = $('#costInput').val()
+inputs.message = $('#msg').val()
+inputs.range = $('#range').val()
+inputs.priority = document.querySelectorAll('input[type="radio"]')
+
+$('reset-inputs').click(
+
+)
+submitButton = document.getElementById('submit-inputs')
+
+resetButton.addEventListener()
+
+submitButton.addEventListener()
+
+function validateInputs() {
+
+}
+
+function resetInputs() {
+
+}
+
+function updateCheckIn() {
+
+}
+
+function updateCheckOut() {
+
+}
+
+function updateDays() {
+
+}
+
+function updateCost() {
+
+}
+
+function invalidInputToaster() {
+
+}
